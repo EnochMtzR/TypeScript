a63bae8a0618a1a5dbdadb4e8635736e9049bd95
Joseph Enoch <josnocpp@protonmail.com>
2019-11-06 10:20:36 -0600

    Explained Type Compatibility.

---
+++ typeCompatibility.ts
@@ -0,0 +1,57 @@
+// interface INamed {
+//   name: string;
+// }
+
+// class Person {
+//   name: string;
+
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+
+//   talk() {
+//     // todo.
+//   }
+// }
+
+// class Animal {
+//   name: string;
+
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+
+//   makeNoise() {
+//     // todo.
+//   }
+// }
+
+// let p: INamed;
+
+// p = new Person("John Doe");
+// p = new Animal("Simba");
+
+// let w = (a: number) => 0;
+// let z = (b: number, s: string) => 0;
+
+// z = w;
+// w = z;
+
+// let x = () => ({ name: "josias" });
+// let y = () => ({ name: "Abner", age: 20 });
+
+// x = y;
+// y = x;
+
+// enum Status {
+//   Pending,
+//   Fullfilled
+// }
+// enum Color {
+//   Red,
+//   Green,
+//   Blue
+// }
+
+// let st = Status.Pending;
+// st = Color.Red;
